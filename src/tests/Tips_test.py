513653cab4f8f82d71303a8b1abf3ecdd54b07c8
Teemu Bergman <teemu.bergman@helsinki.fi>
2021-04-08 15:59:11 +0300

    Koodia siistitty

--- src/tests/Tips_test.py
+++ src/tests/Tips_test.py
@@ -1,13 +1,13 @@
 import unittest
 from tips import Tips
 
-class DB_wrapper_stub():
+
+class DBWrapperStub:
     def __init__(self):
         pass
 
     def insert(self, fields: dict):
         return True
-
 
     # kun varsinaine metodi on valmis muutetaan paluuarvo.
     def display_all_tips(self):
@@ -18,11 +18,9 @@ class DB_wrapper_stub():
         return None
 
 
-
-
 class TestTips(unittest.TestCase):
     def setUp(self):
-        self.tips = Tips(DB_wrapper_stub())
+        self.tips = Tips(DBWrapperStub())
 
     def test_add_tip(self):
         tip = self.tips.add_tip(
@@ -30,13 +28,15 @@ class TestTips(unittest.TestCase):
         self.assertEqual(tip, True)
 
     def test_too_long_name(self):
-        tip = self.tips.add_tip("Arto Paasilinna ja perään aivan liian monta merkkiä että tulee false tästä tarkastuksesta kun on 101!",
-                                "https://www.wsoy.fi/kirja/arto-paasilinna/hurmaava-joukkoitsemurha/9789510426111")
+        tip = self.tips.add_tip(
+            "Arto Paasilinna ja perään aivan liian monta merkkiä että tulee false tästä tarkastuksesta kun on 101!",
+            "https://www.wsoy.fi/kirja/arto-paasilinna/hurmaava-joukkoitsemurha/9789510426111")
         self.assertEqual(tip, False)
 
     def test_too_long_url(self):
         tip = self.tips.add_tip(
-            "Arto Paasilinna", "https://www.wsoy.fi/kirja/arto-paasilinna/hurmaava-joukkoitsemurha/9789510426111/jaturhaa-koodia-peraan-etta-tulee-hylky/kun-merkkeja-tahan-tulee-201-etta-on-juurikin/raja-arvon-ylittava-maara-merkkeja")
+            "Arto Paasilinna",
+            "https://www.wsoy.fi/kirja/arto-paasilinna/hurmaava-joukkoitsemurha/9789510426111/jaturhaa-koodia-peraan-etta-tulee-hylky/kun-merkkeja-tahan-tulee-201-etta-on-juurikin/raja-arvon-ylittava-maara-merkkeja")
         self.assertEqual(tip, False)
 
     def test_non_allowed_character_in_name(self):
