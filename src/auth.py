43c89d07c9d1a3456f494a63f98fc28a4b99c4e8
Teemu Bergman <teemu.bergman@helsinki.fi>
2021-04-15 15:03:22 +0300

    Uloskirjautuminen lisätty

--- src/auth.py
+++ src/auth.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, redirect, url_for, request
-from flask_login import login_required, login_user
+from flask_login import login_required, login_user, logout_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from .models import User
 from . import db
@@ -55,4 +55,5 @@ def register_post():
 @auth.route("/logout")
 @login_required
 def logout():
-    return "Logout"
+    logout_user()
+    return redirect(url_for('auth.login'))
