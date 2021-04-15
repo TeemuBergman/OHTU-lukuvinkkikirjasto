8d81f3d2e35df5e46f8ffde761d30da801302595
Teemu Bergman <teemu.bergman@helsinki.fi>
2021-04-15 14:53:57 +0300

    Rekisteröityminen ja sisäänkirjautuminen implementoitu

--- src/auth.py
+++ src/auth.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, redirect, url_for, request
+from flask_login import login_required, login_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from .models import User
 from . import db
@@ -6,9 +7,25 @@ from . import db
 auth = Blueprint("auth", __name__)
 
 
-@auth.route("/login")
+@auth.route("/")
 def login():
     return render_template("login.html")
+
+
+@auth.route("/", methods = ["GET", "POST"])
+def login_post():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    remember = True if request.form.get('remember') else False
+
+    user = User.query.filter_by(email = email).first()
+
+    if not user or not check_password_hash(user.password, password):
+        return redirect(url_for('auth.login'))
+
+    login_user(user, remember = remember)
+
+    return redirect(url_for('main.add_tips'))
 
 
 @auth.route("/register")
@@ -36,5 +53,6 @@ def register_post():
 
 
 @auth.route("/logout")
+@login_required
 def logout():
     return "Logout"
--- src/main.py
+++ src/main.py
@@ -1,29 +1,31 @@
 from flask import Blueprint, redirect, render_template, request
+from flask_login import login_required
 from .tips import Tips
-from . import db
 
 tips = Tips()
 main = Blueprint('main', __name__)
 
 
-@main.route("/", methods = ["GET", "POST"])
-def home():
+@main.route("/add_tips", methods = ["GET", "POST"])
+@login_required
+def add_tips():
     if request.method == "GET":
         existing_tips = tips.display_all()
         existing_tips = existing_tips[1].fetchall()
-        return render_template("home.html", existing_tips = existing_tips)
+        return render_template("add_tips.html", existing_tips = existing_tips)
 
     if request.method == "POST":
         tip_name = request.form["tip_name"]
         tip_url = request.form["tip_url"]
 
         if tips.add_tip(tip_name, tip_url):
-            return redirect("/")
+            return redirect("/add_tips")
         else:
             return render_template("error.html", message = "Vinkin tallennus epäonnistui.")
 
 
 @main.route("/results", methods = ["GET"])
+@login_required
 def result():
     tip_name = request.args["tip_search"]
     formatted_searches = tips.search_by_writer_name(tip_name)
@@ -32,6 +34,7 @@ def result():
     return render_template("results.html", search_by_name = formatted_searches)
 
 
+# Tarpeellinen Githubin Robot testaukseen
 @main.route("/ping")
 def ping():
     return "Pong"
